e10b81ec0a703b0f1f4e520d2034c5b381fce4b4
boss0703 <prinnykurtis002@gmail.com>
2021-02-24 10:46:38 +0900

    form labelの修正

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,6 @@ class ImageFileForm(forms.ModelForm):
     class Meta:
         model = ImageFileModel
         fields = ('image',)
-        labels = {'image': 'ファイルを選択してください(ドロップ可)', }
         widgets = {
             'image': forms.FileInput(attrs={'class': 'custom-file-input'})
         }
