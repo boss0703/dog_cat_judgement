946680288761fe9cf6b5a0eb3d9fcd20863366ba
boss0703 <prinnykurtis002@gmail.com>
2021-03-22 14:01:36 +0900

    db sqlite3

--- dog_cat_judgement/settings.py
+++ dog_cat_judgement/settings.py
@@ -83,20 +83,27 @@ WSGI_APPLICATION = 'dog_cat_judgement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dog_cat_judgement',
+#         'USER': 'keita',
+#         'PASSWORD': '',
+#         'HOST': 'host',
+#         'PORT': '',
+#     }
+# }
+#
+# # postgres setting
+# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'].update(db_from_env)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dog_cat_judgement',
-        'USER': 'keita',
-        'PASSWORD': '',
-        'HOST': 'host',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# postgres setting
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
