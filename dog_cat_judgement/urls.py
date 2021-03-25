4fc70e2a14cf1e280793291fb42341db634cd785
boss0703 <prinnykurtis002@gmail.com>
2021-03-25 14:49:01 +0900

    url pattern add and debug false

--- dog_cat_judgement/urls.py
+++ dog_cat_judgement/urls.py
@@ -23,9 +23,9 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # else:
-#     urlpatterns += url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
