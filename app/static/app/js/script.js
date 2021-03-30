0732b25f33cc057be930a0a5579292b3e7d23b41
boss0703 <prinnykurtis002@gmail.com>
2021-03-30 14:56:22 +0900

    円グラフのjs少し実装

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,10 +1,19 @@
-$(function(){
-  var $ppc = $('.progress-pie-chart'),
-    percent = parseInt($ppc.data('percent')),
-    deg = 360*percent/100;
-  if (percent > 50) {
-    $ppc.addClass('gt-50');
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["犬", "猫"],
+    datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#95a5a6"
+      ],
+      data: [77.6, 22.4]
+    }]
+  },
+  options: {
+    cutoutPercentage: 65
+  },
+  afterDatasetsDraw: {
   }
-  $('.ppc-progress-fill').css('transform','rotate('+ deg +'deg)');
-  $('.ppc-percents span').html(percent+'%');
 });
