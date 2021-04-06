9b9913290479d469721d028199619bc31e59bb1a
boss0703 <prinnykurtis002@gmail.com>
2021-04-06 11:07:43 +0900

    fix toggle menu

--- app/static/app/js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.navbar-toggler').onclick = function() {
-    // if menu is opening, close menu
-    if(document.getElementById('navbarNav').classList.contains('show')) {
-        document.querySelector('#navbarNav').classList.remove('show');
-    } else {
-      document.querySelector('#navbarNav').classList.add('show');
-    }
-}
