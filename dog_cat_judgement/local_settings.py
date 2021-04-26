d65de61a873bb2ff591c830b2014aa455554cd65
boss0703 <prinnykurtis002@gmail.com>
2021-04-26 11:06:13 +0900

    fix git commits

--- dog_cat_judgement/local_settings.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'mf(*rf=idge(9ollmp&kx0^4ze%*6mkxb@q$bk)(6o&=g-u_)s'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'keita03301995@gmail.com'
-EMAIL_HOST_PASSWORD = 'auly teyx uemm wuxq'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
